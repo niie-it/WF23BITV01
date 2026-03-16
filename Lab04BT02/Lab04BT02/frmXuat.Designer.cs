8a6d46469202741fabb43ff3798e1cf980318db5
HIENLTH <hyhien@gmail.com>
2026-03-16 15:20:46 +0700

    Xu ly tao form

--- Lab04BT02/Lab04BT02/frmXuat.Designer.cs
+++ Lab04BT02/Lab04BT02/frmXuat.Designer.cs
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // frmXuat
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "frmXuat";
             this.Text = "frmXuat";
+            this.Load += new System.EventHandler(this.frmXuat_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- Lab04BT02/Lab04BT02/frmXuat.cs
+++ Lab04BT02/Lab04BT02/frmXuat.cs
@@ -18,5 +18,39 @@ namespace Lab04BT02
         {
             InitializeComponent();
         }
+
+        const int CellSize = 50;
+        const int Spacing = 20;
+        private void frmXuat_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < SoDong; i++)
+            {
+                for (int j = 0; j < SoCot; j++)
+                {
+                    var btn = new Button();
+                    btn.Size = new Size(CellSize, CellSize);
+                    btn.Left = Spacing + j * (CellSize + Spacing);
+                    btn.Top = Spacing + i * (CellSize + Spacing);
+                    btn.Text = $"({i},{j})";
+                    btn.Click += new EventHandler(XuLyClickButton);
+                    btn.MouseHover += new EventHandler(XuLyHoverChuot);
+                    this.Controls.Add(btn);
+                }
+            }
+
+            //ép kích thước form để vừa với số lượng nút
+            this.Width = Spacing + SoCot * (CellSize + Spacing) + 15;
+            this.Height = Spacing + SoDong * (CellSize + Spacing) + 30;
+        }
+
+        private void XuLyClickButton(object sender, EventArgs e)
+        {
+            MessageBox.Show($"Bạn đã click vào nút {((Button)sender).Text}", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void XuLyHoverChuot(object sender, EventArgs e)
+        {
+            this.Text = $"Bạn đang hover chuột vào nút {((Button)sender).Text}";
+        }
     }
 }
